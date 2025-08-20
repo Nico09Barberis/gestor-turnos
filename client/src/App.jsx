f7bece2f77123212f62e0b89696beffae9e88c9d
Nico Barberis <nico09barberis@gmail.com>
2025-08-20 17:54:56 -0300

    ajuste en App.jsx para empezar a ver las pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+
+import LoginPage from "./pages/Auth/LoginPage";
+import RegisterPage from "./pages/Auth/RegisterPage";
+
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-100">
-      <h1 className="text-3xl font-bold text-blue-700">Frontend funcionando!</h1>
-    </div>
-  )
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/* Aquí luego agregaremos el dashboard */}
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
 export default App;
