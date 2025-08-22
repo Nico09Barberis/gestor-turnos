eba12c994eda390508ed595706ac90ce82b79438
Nico Barberis <nico09barberis@gmail.com>
2025-08-22 09:42:12 -0300

    ajuste para permitir el scroll vertical

--- client/src/pages/Users/AppointmentsListPage.jsx
+++ client/src/pages/Users/AppointmentsListPage.jsx
@@ -2,7 +2,7 @@ import MyAppointments from "../../components/AppointmentList";
 
 const AppointmentsListPage = () => {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
+    <div className="min-h-screen bg-gray-100 p-6 overflow-y-auto">
       <MyAppointments />
     </div>
   );
