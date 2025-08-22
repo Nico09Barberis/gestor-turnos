69fa5286a4a26b52804434f8f776075b4f734e5d
Nico Barberis <nico09barberis@gmail.com>
2025-08-22 09:50:14 -0300

    agregue las nuevas rutas y configurando en services/user para manejar el form

--- client/src/services/user.js
+++ client/src/services/user.js
@@ -5,3 +5,24 @@ export const getBarbers = async () => {
   const res = await API.get("/users/barbers");
   return res.data; // devuelve un array de { _id, name }
 };
+
+
+// Obtener perfil del usuario logueado
+export const getMyProfile = async () => {
+  const { data } = await API.get("/users/me");
+  return data;
+};
+
+
+// Actualizar nombre y email
+export const updateProfile = async (profile) => {
+  const { data } = await API.put("/users/me", profile);
+  return data;
+};
+
+
+// Cambiar contraseña
+export const changePassword = async (passwords) => {
+  const { data } = await API.put("/users/me/password", passwords);
+  return data;
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,12 @@
 // routes/users.js
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware.js";
-import { getBarbers } from "../controllers/userController.js";
+import { getBarbers, getMyProfile, updateProfile, changePassword } from "../controllers/userController.js";
 
 const router = express.Router();
 router.get("/barbers", authMiddleware, getBarbers);
+router.get("/me", authMiddleware, getMyProfile);
+router.put("/me", authMiddleware, updateProfile);
+router.put("/me/password", authMiddleware, changePassword);
 
 export default router;
